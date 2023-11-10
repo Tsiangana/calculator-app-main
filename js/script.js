b55744db8cddcf4a5243dcc73ecf15d635c8a209
Tsiangana <pedrozau250@gmail.com>
2023-11-10 19:09:05 +0000

    Calculater 100% done

---
+++ js/script.js
@@ -0,0 +1,221 @@
+
+/* Variaveis globais */
+
+let container = document.getElementById('container');
+let visor = document.getElementById('visor');
+let keypad = document.getElementById('keypad');
+let color = document.getElementById('color');
+let color1 = document.getElementById('color1');
+
+let button1 = document.getElementById('button1');
+let button2 = document.getElementById('button2');
+let button3 = document.getElementById('button3');
+
+
+/* inicio da troca de cor do primeiro thema */
+
+    document.getElementById("radio1").addEventListener("click", function() {
+
+        container.style.background = "";
+        visor.style.background = "";
+        visor.style.color = "";
+        keypad.style.background = "";
+        color.style.color = "";
+        color1.style.color = "";
+
+        button1.style.background = "";
+        button2.style.background = "";
+        button3.style.background = "";
+
+        b1.style.background = "";
+        b1.style.color = "";
+
+        b2.style.background = "";
+        b2.style.color = "";
+
+        b3.style.background = "";
+        b3.style.color = "";
+
+        b4.style.background = "";
+        b4.style.color = "";
+
+        b5.style.background = "";
+        b5.style.color = "";
+
+        b6.style.background = "";
+        b6.style.color = "";
+
+        b7.style.background = "";
+        b7.style.color = "";
+
+        b8.style.background = "";
+        b8.style.color = "";
+
+        b9.style.background = "";
+        b9.style.color = "";
+
+        b10.style.background = "";
+        b10.style.color = "";
+
+        b11.style.background = "";
+        b11.style.color = "";
+
+        b12.style.background = "";
+        b12.style.color = "";
+
+        b13.style.background = "";
+        b13.style.color = "";
+
+        b14.style.background = "";
+        b14.style.color = "";
+
+        b0.style.background = "";
+        b0.style.color = "";
+    })
+
+/* fim da troca de cor do primeiro thema */
+
+
+
+/* inicio da troca de cor do segundo thema */
+
+document.getElementById("radio2").addEventListener("click", function() {
+
+    container.style.background = "hsl(0, 0%, 90%)";
+    visor.style.background = "hsl(0, 0%, 93%)";
+    visor.style.color = "hsl(198, 20%, 13%)";
+    keypad.style.background = "hsl(0, 5%, 81%)";
+    color.style.color = "hsl(198, 20%, 13%)";
+    color1.style.color = "hsl(198, 20%, 13%)";
+
+    button1.style.background = "hsl(185, 42%, 37%)";
+    button2.style.background = "hsl(185, 42%, 37%)";
+    button3.style.background = "hsl(25, 98%, 40%)";
+
+    b1.style.background = "";
+    b1.style.color = "";
+
+    b2.style.background = "";
+    b2.style.color = "";
+
+    b3.style.background = "";
+    b3.style.color = "";
+
+    b4.style.background = "";
+    b4.style.color = "";
+
+    b5.style.background = "";
+    b5.style.color = "";
+
+    b6.style.background = "";
+    b6.style.color = "";
+
+    b7.style.background = "";
+    b7.style.color = "";
+
+    b8.style.background = "";
+    b8.style.color = "";
+
+    b9.style.background = "";
+    b9.style.color = "";
+
+    b10.style.background = "";
+    b10.style.color = "";
+
+    b11.style.background = "";
+    b11.style.color = "";
+
+    b12.style.background = "";
+    b12.style.color = "";
+
+    b13.style.background = "";
+    b13.style.color = "";
+
+    b14.style.background = "";
+    b14.style.color = "";
+
+    b0.style.background = "";
+    b0.style.color = "";
+})
+
+/* fim da troca de cor do segundo thema */
+
+/* inicio da troca de cor do terceiro thema */
+
+let b1 = document.getElementById('b1');
+let b2 = document.getElementById('b2');
+let b3 = document.getElementById('b3');
+let b4 = document.getElementById('b4');
+let b5 = document.getElementById('b5');
+let b6 = document.getElementById('b6');
+let b7 = document.getElementById('b7');
+let b8 = document.getElementById('b8');
+let b9 = document.getElementById('b9');
+let b10 = document.getElementById('b10');
+let b11 = document.getElementById('b11');
+let b12 = document.getElementById('b12');
+let b13 = document.getElementById('b13');
+let b14 = document.getElementById('b14');
+let b0 = document.getElementById('b0');
+
+document.getElementById("radio3").addEventListener("click", function() {
+
+    container.style.background = "hsl(268, 75%, 9%)";
+    visor.style.background = "hsl(268, 71%, 12%)";
+    visor.style.color = "hsl(52, 100%, 62%)";
+    keypad.style.background = "hsl(268, 71%, 12%)";
+    color.style.color = "hsl(52, 100%, 62%)";
+    color1.style.color = "hsl(52, 100%, 62%)";
+
+    button1.style.background = "hsl(281, 89%, 26%)";
+    button2.style.background = "hsl(281, 89%, 26%)";
+    button3.style.background = "hsl(176, 100%, 44%)";
+
+    b1.style.background = "hsl(268, 47%, 21%)";
+    b1.style.color = "hsl(52, 100%, 62%)";
+
+    b2.style.background = "hsl(268, 47%, 21%)";
+    b2.style.color = "hsl(52, 100%, 62%)";
+
+    b3.style.background = "hsl(268, 47%, 21%)";
+    b3.style.color = "hsl(52, 100%, 62%)";
+
+    b4.style.background = "hsl(268, 47%, 21%)";
+    b4.style.color = "hsl(52, 100%, 62%)";
+
+    b5.style.background = "hsl(268, 47%, 21%)";
+    b5.style.color = "hsl(52, 100%, 62%)";
+
+    b6.style.background = "hsl(268, 47%, 21%)";
+    b6.style.color = "hsl(52, 100%, 62%)";
+
+    b7.style.background = "hsl(268, 47%, 21%)";
+    b7.style.color = "hsl(52, 100%, 62%)";
+
+    b8.style.background = "hsl(268, 47%, 21%)";
+    b8.style.color = "hsl(52, 100%, 62%)";
+
+    b9.style.background = "hsl(268, 47%, 21%)";
+    b9.style.color = "hsl(52, 100%, 62%)";
+
+    b10.style.background = "hsl(268, 47%, 21%)";
+    b10.style.color = "hsl(52, 100%, 62%)";
+
+    b11.style.background = "hsl(268, 47%, 21%)";
+    b11.style.color = "hsl(52, 100%, 62%)";
+
+    b12.style.background = "hsl(268, 47%, 21%)";
+    b12.style.color = "hsl(52, 100%, 62%)";
+
+    b13.style.background = "hsl(268, 47%, 21%)";
+    b13.style.color = "hsl(52, 100%, 62%)";
+
+    b14.style.background = "hsl(268, 47%, 21%)";
+    b14.style.color = "hsl(52, 100%, 62%)";
+
+    b0.style.background = "hsl(268, 47%, 21%)";
+    b0.style.color = "hsl(52, 100%, 62%)";
+
+})
+
+/* fim da troca de cor do terceiro thema */
